b39f2a91a7504e227bc98a50da44321564345c83
dav-vn <vartanyandg@gmail.com>
2023-04-28 23:22:22 +0300

    fix

--- Are they the same (6).js
+++ Are they the same (6).js
@@ -1,4 +1,7 @@
 function comp(array1, array2){
+    if (array1 == null || array2 == null) {
+        return false;
+    }
     aSorted = array1.sort((a,b) =>  a - b);
     bSorted = array2.sort((a,b) =>  a - b);
 
